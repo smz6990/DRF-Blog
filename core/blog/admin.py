a1857c22fd1f63a5101936002dd7979958b32023
saleh <saleh.mohammadzadeh@gmail.com>
2022-08-17 21:58:47 +0430

    adding author field to Post table in blog app and register it in admin to show it in admin panel

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from blog.models import Category,Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    creating class to modify the admin panel
+    """
+    date_hierarchy = 'created_date'
+    list_display = ['author', 'title', 'status', 'category', 'published_date', 'created_date']
+    list_filter = ['author', 'status', 'category']
+    search_fields = ['author', 'title', 'content']
+    
+    
+# registering the Category model to show it in admin panel
+admin.site.register(Category)
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+# getting the user model object
+User = get_user_model()
+
 class Post(models.Model):
     """
     This is a table in our DB, Post is table name and below attributes are our field in our table
     """
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     content = models.TextField()
     status = models.BooleanField(default=False)
@@ -15,8 +19,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
-    class Meta:
-        
+    class Meta: 
         ordering = ['-published_date','-created_date']
         
     def __str__(self):
@@ -26,7 +29,6 @@ class Category(models.Model):
     """
     This is the Category table with name field in our database
     """
-    
     name = models.CharField(max_length=255)
     
     def __str__(self):
