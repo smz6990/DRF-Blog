d268d3a4ffeeaa2603b87a79db4c0518b7025c9a
saleh <saleh.mohammadzadeh@gmail.com>
2022-08-17 17:20:08 +0430

    creating blog model and custom user model and manager for it in accounts
    creating class for post and category in blog models , creating custom user class and manager for it in accounts app

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from accounts.managers import UserManager
 
-# Create your models here.
+class User(AbstractBaseUser,PermissionsMixin):
+    """
+    Creating a class that represent the custom User model for
+    authentication
+    """
+    
+    email = models.EmailField(unique=True,max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    # is_verified = models.BooleanField(default=False)
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.email
--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """
+    This is a table in our DB, Post is table name and below attributes are our field in our table
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    image = models.ImageField(blank=True,null=True)
+    category = models.ManyToManyField('Category',on_delete=models.SET_NULL,null=True)
+    published_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        
+        ordering = ['-published_date','-created_date']
+        
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    """
+    This is the Category table with name field in our database
+    """
+    
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
