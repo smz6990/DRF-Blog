1b1ef5d96e3f36b74ec02c24098ce4492d81b4c0
saleh <saleh.mohammadzadeh@gmail.com>
2022-08-17 19:50:06 +0430

    creating profile model and registering it to admin panel

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import User
+from accounts.models import User,Profile
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     """
-    creating a class to show the fields in admin panel
+    creating a class to show the User fields in admin panel
     """
-    
     model = User
     list_display = ['email','is_superuser','is_staff','is_active']
     list_filter = ['email','is_superuser','is_staff','is_active']
@@ -24,3 +23,12 @@ class CustomUserAdmin(UserAdmin):
             'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')}
         ),
     )
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """
+        creating a class to show the Profile fields in admin panel
+    """
+    list_display = ['first_name','last_name','user','created_date','updated_date']
+    search_fields = ['first_name','last_name','user']
+    ordering = ['created_date']
--- core/accounts/models.py
+++ core/accounts/models.py
@@ -6,21 +6,33 @@ class User(AbstractBaseUser,PermissionsMixin):
     """
     Creating a class that represent the custom User model for
     authentication
-    """
-    
+    """    
     email = models.EmailField(unique=True,max_length=255)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     # is_verified = models.BooleanField(default=False)
-    
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
-    
     objects = UserManager()
     
     def __str__(self):
         return self.email
+    
+class Profile(models.Model):
+    """
+    This is a class that represent the profile for every account in our site
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, null=True)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.email
