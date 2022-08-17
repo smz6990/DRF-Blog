f4acca2c8bf04026697ade33410175c2ac9f8830
saleh <saleh.mohammadzadeh@gmail.com>
2022-08-17 19:04:05 +0430

    register CustomUserAdmin for User class

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    creating a class to show the fields in admin panel
+    """
+    
+    model = User
+    list_display = ['email','is_superuser','is_staff','is_active']
+    list_filter = ['email','is_superuser','is_staff','is_active']
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active','is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser')}
+        ),
+    )
