e4084f21dd35e049a5f2efaab52bd2a56e1fc81a
saleh <saleh.mohammadzadeh@gmail.com>
2022-10-03 19:26:56 +0330

    adding tests for views in website app

--- core/website/tests/test_urls.py
+++ core/website/tests/test_urls.py
@@ -4,18 +4,18 @@ from .. import views
 
 
 class TestWebsiteUrlsResolve:
-    def test_index_view_url(self):
+    def test_url_index_view(self):
         url = reverse("website:index")
         assert resolve(url).func.view_class == views.IndexView
 
-    def test_about_view_url(self):
+    def test_url_about_view(self):
         url = reverse("website:about")
         assert resolve(url).func.view_class == views.AboutView
 
-    def test_contact_view_url(self):
+    def test_url_contact_view(self):
         url = reverse("website:contact")
         assert resolve(url).func.view_class == views.ContactView
 
-    def test_newsletter_view_url(self):
+    def test_url_newsletter_view(self):
         url = reverse("website:newsletter")
         assert resolve(url).func.view_class == views.NewsletterView
---
+++ core/website/tests/test_views.py
@@ -0,0 +1,47 @@
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+
+@pytest.fixture
+def basic_user_client():
+    return Client()
+
+
+@pytest.mark.django_db
+class TestWebsiteView:
+    def test_index_view_get_response_200(self, basic_user_client):
+        url = reverse("website:index")
+        response = basic_user_client.get(url)
+        assert response.status_code == 200
+
+    def test_about_view_get_response_200(self, basic_user_client):
+        url = reverse("website:about")
+        response = basic_user_client.get(url)
+        assert response.status_code == 200
+
+    def test_contact_view_get_response_200(self, basic_user_client):
+        url = reverse("website:contact")
+        response = basic_user_client.get(url)
+        assert response.status_code == 200
+
+    def test_contact_view_post_response_200(self, basic_user_client):
+        url = reverse("website:contact")
+        data = {
+            "name": "test",
+            "email": "test@test.com",
+            "subject": "test subject",
+            "message": "test message",
+            "captcha": 1,
+        }
+        response = basic_user_client.post(url, data)
+        assert response.status_code == 200
+
+    def test_newsletter_view_post_response_302(self, basic_user_client):
+        """after successful post , redirects to the index page"""
+        url = reverse("website:newsletter")
+        data = {
+            "email": "test@test.com",
+        }
+        response = basic_user_client.post(url, data)
+        assert response.status_code == 302
