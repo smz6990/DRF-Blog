503c1209aaf2a2a75c2895cd9e32e2b4a27b7d91
saleh <saleh.mohammadzadeh@gmail.com>
2022-10-05 13:05:07 +0330

    adding tests for views in api blog app

--- core/blog/api/v1/tests/test_api_urls.py
+++ core/blog/api/v1/tests/test_api_urls.py
@@ -6,21 +6,21 @@ from .. import views
 
 @pytest.mark.django_db
 class TestBlogApiUrls:
-    def test_blog_post_list_create_api_url(self):
+    def test_blog_post_list_api_url(self):
         url = reverse("blog:api-v1:post-list")
         assert (
             resolve(url).func.view_class
             == views.BlogIndexListCreateAPIView
         )
 
-    def test_blog_post_single_RUD_api_url(self):
+    def test_blog_post_single_api_url(self):
         url = reverse("blog:api-v1:post-single", kwargs={"pk": 1})
         assert (
             resolve(url).func.view_class
             == views.BlogSingleRetrieveUpdateDeleteAPIView
         )
 
-    def test_category_list_create_api_url(self):
+    def test_category_api_url(self):
         url = reverse("blog:api-v1:category")
         assert (
             resolve(url).func.view_class == views.CategoryListCreateAPIView
